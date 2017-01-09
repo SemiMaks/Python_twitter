3b2e09d55bc14340d40154e3bba7a48b037779bf
maksim <semimaks@mail.ru>
2017-01-09 19:09:21 +0300

    Правка кода

--- twitter.py
+++ twitter.py
@@ -4,11 +4,15 @@ import tweepy
 consumer_key = 'JsV6FCCUsCqkKsbc2LzsBLlh2'
 consumer_secret = 'flC0NzGyKsrnb8vTLJvUtUoaDkw2UPuchPlgmk3VeFdKKsVkNo'
 access_token = '2335158726-XBiP8R9HT7ijIyl0RvHx2UWtmH8gsZYYhXILCY9'
-access_token secret = 'hOwHDL2TlQvPS9N0tfNoWd6hV78J8O7xnrpaHGNTFQ45k'
+access_token_secret = 'hOwHDL2TlQvPS9N0tfNoWd6hV78J8O7xnrpaHGNTFQ45k'
 
 # настройки OAuth API
-auth = tweepy.OAuthHandler(JsV6FCCUsCqkKsbc2LzsBLlh2, flC0NzGyKsrnb8vTLJvUtUoaDkw2UPuchPlgmk3VeFdKKsVkNo)
-auth.set_access_token(2335158726-XBiP8R9HT7ijIyl0RvHx2UWtmH8gsZYYhXILCY9, hOwHDL2TlQvPS9N0tfNoWd6hV78J8O7xnrpaHGNTFQ45k)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # текст поста для учетной записи Twitter
+tweet = 'Пробная запись!'
+api.update_status(status=tweet)
+
+print ('tweet')
